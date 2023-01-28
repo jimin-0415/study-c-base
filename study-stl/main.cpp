d7cae39701488fc148325e53798ab71ea86f0e16
jimin Kim <programmer0415@gmail.com>
2023-01-28 23:59:06 +0900

    Chore: 서브 프로젝트 생성

--- study-stl/main.cpp
+++ study-stl/main.cpp
@@ -1,11 +1,11 @@
-﻿// study-c-base.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+// study-c-base.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Test" << std::endl;
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
